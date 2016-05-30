bc1a4a1d92f915716238be8f74f94b102266c9c2
Wardrt <ryanward1@btinternet.com>
2016-05-30 10:15:44 +0100

    Saving users, but not saving current user so not being logged in.

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.use('/api', expressJWT({ secret: secret })
   .unless({
     path: [
       { url: '/api/login', methods: ['POST'] },
-      { url: '/api/register', methods: ['POST'] }
+      { url: '/api/register', methods: ['POST'] },
+      { url: '/api/users', methods: ['GET'] }
     ]
 }));
 
--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'port': process.env.PORT || 3000,
-  'secret': process.env.JWT_SECRET || 'sadakjd238hISD9h#!)smndasdkh',
+  'secret': process.env.JWT_SECRET || 'uygfhjhgcjyc765464$@tddrr5',
   'database': process.env.MONGODB_URL || 'mongodb://localhost:27017/twitch-roulette'
 };
