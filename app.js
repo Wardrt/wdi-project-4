77d8edb20dc237d5c3702b0e8a47e5c6fec4ef16
Wardrt <ryanward1@btinternet.com>
2016-05-30 09:43:44 +0100

    first commit.

---
+++ app.js
@@ -0,0 +1,10 @@
+var express        = require("express");
+var cors           = require("cors");
+var morgan         = require("morgan");
+var bodyParser     = require("body-parser");
+var mongoose       = require("mongoose");
+var methodOverride = require("method-override");
+
+
+
+var app            = express();
