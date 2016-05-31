26c8a56fd06ded9924fb0872fc0c2b59bcdb48b6
Wardrt <ryanward1@btinternet.com>
2016-05-31 18:54:02 +0100

    chat showing up on different browsers, now to give user name.

--- app.js
+++ app.js
@@ -64,8 +64,7 @@ app.get("/*", function(req, res) {
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log(msg);
-    socket.emit('message', msg);
+    io.emit('message', msg);
   });
 });
 
