267b7646a01c221ce2a02f4174efd8f641f67d3f
Wardrt <ryanward1@btinternet.com>
2016-06-02 15:55:22 +0100

    on click login loads a modal.

--- public/js/app.js
+++ public/js/app.js
@@ -47075,6 +47075,26 @@ $(document).ready(function() {
   });
 });
 
+// ******** MODAL ********
+
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
 angular
   .module("twitchRoulette")
   .config(Interceptor);
